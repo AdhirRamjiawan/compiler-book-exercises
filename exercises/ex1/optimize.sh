4ae41cec45abeecd5792d74224f1b82f6b5af335
Adhir Ramjiawan <adhirramjiawan0@gmail.com>
2020-07-22 20:41:11 +0200

    added optimize to ex 1

---
+++ exercises/ex1/optimize.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#-O0 (default)
+#-O1 or -O (optimize for code size and execution time, compile time +)
+#-O2 (optimize for code size and execution time, compile time ++)
+#-O3 (optimize for code size and execution time, compile time +++)
+#-Os (optimize for code size, compile time ++)
+
+gcc -O0 main.c
