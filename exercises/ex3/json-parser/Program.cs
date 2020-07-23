9ae56fdb4cc0185780f598dc87869a9c40bb4343
Adhir Ramjiawan <adhirramjiawan0@gmail.com>
2020-07-23 18:50:57 +0200

    added null property support

--- exercises/ex3/json-parser/Program.cs
+++ exercises/ex3/json-parser/Program.cs
@@ -13,14 +13,14 @@ namespace json_parser
             // Value => "test" | 123 | 23.2 | {} | [] | true | false | null
 
             //string jsonInput = "{\"menu\": {\"id\": \"file\",\"value\": \"File\",\"popup\": {\"menuitem\": [{\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"},{\"value\": \"Open\", \"onclick\": \"OpenDoc()\"},{\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}]}}}";
-            string jsonInput = "{\"widget\": {\"debug\": false, \"listOfStuff\":[123, 123.44, \"test\", {}],\"window\": { \"fullscreen\": true, \"title\": \"Sample Konfabulator Widget\",\"name\": \"main_window\",\"width\": 500,\"height\": 500},\"image\": { \"src\": \"Images/Sun.png\",\"name\": \"sun1\",\"hOffset\": 250,        \"vOffset\": 250,        \"alignment\": \"center\"    },    \"text\": {        \"data\": \"Click Here\",        \"size\": 36,        \"style\": \"bold\",        \"name\": \"text1\",        \"hOffset\": 250,        \"vOffset\": 100,        \"alignment\": \"center\",        \"onMouseUp\": \"sun1.opacity = (sun1.opacity / 100) * 90;\"    }}}    ";
+            string jsonInput = "{\"widget\": {\"handle\":null, \"debug\": false, \"listOfStuff\":[123, 123.44, \"test\", {}],\"window\": { \"fullscreen\": true, \"title\": \"Sample Konfabulator Widget\",\"name\": \"main_window\",\"width\": 500,\"height\": 500},\"image\": { \"src\": \"Images/Sun.png\",\"name\": \"sun1\",\"hOffset\": 250,        \"vOffset\": 250,        \"alignment\": \"center\"    },    \"text\": {        \"data\": \"Click Here\",        \"size\": 36,        \"style\": \"bold\",        \"name\": \"text1\",        \"hOffset\": 250,        \"vOffset\": 100,        \"alignment\": \"center\",        \"onMouseUp\": \"sun1.opacity = (sun1.opacity / 100) * 90;\"    }}}    ";
             string tmp = string.Empty;
             string numbers = "0123456789";
             string lastToken = string.Empty;
 
             // use a while loop instead
             // to get n-number of nested objects/arrays etc need to use recursion.
-            
+
             for (int i = 0; i < jsonInput.Length; i++)
             {
                 char c = jsonInput[i];
@@ -45,6 +45,23 @@ namespace json_parser
                         if (c2 == '"')
                         {
                             lastToken = "[STRING_END]";
+                            Console.WriteLine(lastToken);
+                            i = j;
+                            break;
+                        }
+                    }
+                }
+                else if ((c == 'n') && lastToken == "[COLON]")
+                {
+                    lastToken = "[NULL_START]";
+                    Console.WriteLine(lastToken);
+
+                    for (int j = i + 1; j < jsonInput.Length; j++)
+                    {
+                        char c2 = jsonInput[j];
+                        if (c2 == 'l')
+                        {
+                            lastToken = "[NULL_END]";
                             Console.WriteLine(lastToken);
                             i = j;
                             break;
